529d9fabd788fad88f872cc7a472694137d22a58
espirapaco23 <102536675+espirapaco23@users.noreply.github.com>
2022-12-01 05:21:00 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,80 @@
+let canvas = document.getElementById("canvas");
+let canvasContext = canvas.getContext("2d");
+
+let allRains = [];
+let defaultRainWidth = 2;
+let defaultRainHeight = 15;
+let fps = 60;
+let maximumRainCount = 300;
+let rainFrame = 20;
+let maximumRainFrame = 25;
+
+class Rain {
+  constructor(width, height, direction, posX, posY, speed, color) {
+    this.width = width;
+    this.height = height;
+    this.direction = direction;
+    this.posX = posX;
+    this.posY = posY;
+    this.speed = speed;
+    this.color = color;
+  }
+  move() {
+    this.posX += Math.sin((this.direction * Math.PI) / 2) * this.speed;
+    this.posY += Math.cos((this.direction * Math.PI) / 2) * this.speed;
+  }
+  draw() {
+    canvasContext.rotate(-this.direction);
+    canvasContext.fillStyle = this.color;
+    canvasContext.fillRect(this.posX, this.posY, this.width, this.height);
+    canvasContext.rotate(this.direction);
+  }
+}
+
+let createRect = (x, y, width, height, color) => {
+  canvasContext.fillStyle = color;
+  canvasContext.fillRect(x, y, width, height);
+};
+
+let gameLoop = () => {
+  setInterval(show, 1000 / fps);
+};
+
+let show = () => {
+  update();
+  draw();
+};
+
+let update = () => {
+  canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+  let rainFrame = 0;
+  while (allRains.length < maximumRainCount && maximumRainFrame > rainFrame) {
+    let distanceFromCam = Math.random();
+    let rain = new Rain(
+      defaultRainWidth * (2 - distanceFromCam),
+      defaultRainHeight * (2 - distanceFromCam),
+      Math.random() / 20,
+      Math.random() * canvas.width,
+      -50,
+      (2 - distanceFromCam) * 10,
+      "rgba(192, 192, 192, " + (1 - distanceFromCam) + " )"
+    );
+    allRains.push(rain);
+    rainFrame++;
+  }
+
+  for (let i = 0; i < allRains.length; i++) {
+    allRains[i].move();
+    if (allRains[i].posY > canvas.height || allRains[i].posX > canvas.width) {
+      allRains.splice(i, 1);
+    }
+  }
+};
+
+let draw = () => {
+  allRains.forEach((rain) => {
+    rain.draw();
+  });
+};
+
+gameLoop();
